e5b1636d57f666fd3aa5db346bb5b8e8876edb3a
SamuelFecomercio <156712738+SamuelFecomercio@users.noreply.github.com>
2024-03-28 13:13:15 -0300

    add menu burger

--- src/components/headerComponent/headerComponent.js
+++ src/components/headerComponent/headerComponent.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 //css
 import "./headerComponent.css";
+
+//bibliotecas
+import { slide as Menu } from "react-burger-menu";
 
 //imagens
 import Logo from "../../assets/img/logo/logo-esportedasorte.png";
@@ -12,12 +15,26 @@ function loginRedirect() {
 }
 
 //Conteúdo Principal
-function headerComponent() {
+function HeaderComponent() {
+  // Estado para controlar la visibilidad del menú
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Función para abrir el menú
+  const openMenu = () => {
+    setIsMenuOpen(true);
+  };
+
+  // Función para cerrar el menú
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <main className="main-headerComponent">
-
       <div className="logo-header-container">
-        <a href="/"><img className="logo-header" src={Logo} alt="Logo Esporte da sorte" /></a>
+        <a href="/">
+          <img className="logo-header" src={Logo} alt="Logo Esporte da sorte" />
+        </a>
       </div>
 
       <div>
@@ -41,8 +58,36 @@ function headerComponent() {
           </ul>
         </a>
       </div>
+
+      {/* Menú de hamburguesa que se muestra solo cuando isMenuOpen es true */}
+      <Menu styles={"height= 2rem ;"}
+        isOpen={isMenuOpen}
+        onStateChange={(state) => setIsMenuOpen(state.isOpen)}
+      >
+        <ul className="ul-headerComponent">
+          <li>
+            <a href="/">Início</a>
+          </li>
+          <br></br>
+          <li>
+            <a href="/">Rifas</a>
+          </li>
+          <br></br>
+          <li>
+            <a href="/">Meus Bilhetes</a>
+          </li>
+          <br></br>
+          <li>
+            <a href="/">Contatos</a>
+          </li>
+          <br></br>
+          <li>
+            <button onClick={loginRedirect}>Login</button>
+          </li>
+        </ul>
+      </Menu>
     </main>
   );
 }
 
-export default headerComponent;
+export default HeaderComponent;
